27eb31378f33e6d36ec6a4694d4b68a19eabae07
LunaKawa <69447518+LunaKawa@users.noreply.github.com>
2020-11-14 12:37:39 -0800

    deleted loader spinner

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import WeatherData from "./WeatherData";
-import Loader from 'react-loader-spinner'
+
 export default function SearchEngine() {
   let [city, setCity] = useState("");
   let [message, showMessage] = useState("");
@@ -20,13 +20,7 @@ export default function SearchEngine() {
         <input type="submit" />
       </form>
       <h2>{message}</h2>
-      <Loader
-         type="TailSpin
-         "
-         color="#00BFFF"
-         height={100}
-         width={100}
-         timeout={3000}/>
+    
     </div>
   );
 }
