7d90f7e08f22eab26f1f75c762f5a164892de308
LunaKawa <69447518+LunaKawa@users.noreply.github.com>
2020-11-22 18:24:33 -0800

    import Bootstrap components and added html

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,64 @@
 import React from "react";
 import SearchEngine from "./SearchEngine";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Image from "react-bootstrap/Image";
+import Forecast from "./Forecast";
 import "./weather.css";
 
 export default function Weather() {
-  return <h1>Hello</h1>;
+  return (
+    <Card>
+      <Card.Body>
+        <Row>
+          <Col>
+            <ul>
+              <li>Redondo Beach, CA</li>
+              <li>Sun 22 Nov</li>
+              <li>Clear Skies</li>
+            </ul>
+            <div className="current-weather">
+              <Image src=" https://ssl.gstatic.com/onebox/weather/64/sunny.png" />{" "}
+              <strong>23°C</strong>
+            </div>
+            <Row>
+              <Col>
+                <div>
+                  <p>Feels: 24°C</p>
+                </div>
+                <div>
+                  <p>High: 25°C</p>
+                </div>
+                <div>
+                  <p>Low: 21°C</p>
+                </div>
+              </Col>
+            </Row>
+          </Col>
+          <Col>
+            <SearchEngine />
+            <div>
+              <ul>
+                <li>
+                  Wind:<strong>2 m/s</strong>
+                </li>
+                <li>
+                  Humidity:<strong> 15%</strong>
+                </li>
+                <li>
+                  Pressure:<strong>1061 hPa</strong>
+                </li>
+              </ul>
+            </div>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Forecast />
+          </Col>
+        </Row>
+      </Card.Body>
+    </Card>
+  );
 }
