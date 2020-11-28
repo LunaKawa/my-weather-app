91068ef88c3a6e1fecf915b2af77c5352a971578
LunaKawa <69447518+LunaKawa@users.noreply.github.com>
2020-11-27 18:42:47 -0800

    update forecast placeholder

--- src/Forecast.js
+++ src/Forecast.js
@@ -3,17 +3,5 @@ import CardDeck from "react-bootstrap/CardDeck";
 import Card from "react-bootstrap/Card";
 
 export default function Forecast() {
-  return (
-    <CardDeck>
-      <Card>
-        <Card.Img variant="top" src="holder.js/100px160" />
-        <Card.Body>
-          <Card.Title>21 23 </Card.Title>
-        </Card.Body>
-        <Card.Footer>
-          <small className="text-muted">Last updated 3 mins ago</small>
-        </Card.Footer>
-      </Card>
-    </CardDeck>
-  );
+  return <div>Hello World</div>;
 }
