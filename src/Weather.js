e48a242e115268822cab66563ef6563de27cc4fa
LunaKawa <69447518+LunaKawa@users.noreply.github.com>
2020-11-29 10:59:24 -0700

    update weather.js to integrate api

--- src/Weather.js
+++ src/Weather.js
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import SearchEngine from "./SearchEngine";
 import Card from "react-bootstrap/Card";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Image from "react-bootstrap/Image";
 import Forecast from "./Forecast";
+import axios from "axios";
 import "./weather.css";
 
-export default function Weather() {
+export default function Weather(props) {
+  let [temp, setTemp] = useState(null);
+  let [description, setDescription] = useState(null);
+  let [humidity, setHumidity] = useState(null);
+  let [wind, setWind] = useState(null);
+  let [icon, setIcon] = useState(null);
+  function showWeather(response) {
+    setTemp(response.data.main.temp);
+    setDescription(response.data.weather[0].description);
+    setHumidity(response.data.main.humidity);
+    setWind(response.data.wind.speed);
+    setIcon(response.data.weather[0].icon);
+  }
+  let city = "Tokyo";
+  let apiKey = "7a608a2c9f2ddbe4a22465f08c0c779a";
+  let iconUrl = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(url).then(showWeather);
   return (
     <Card className="weather">
       <Card.Body>
@@ -23,9 +41,9 @@ export default function Weather() {
         <Row>
           <Col>
             <div className="current-weather">
-              <h3>Clear Sky</h3>
-              <Image src=" https://ssl.gstatic.com/onebox/weather/64/sunny.png" />{" "}
-              <strong className="current-temp">23°C</strong>
+              <h3>{description}</h3>
+              <Image src={iconUrl} alt={description} />
+              <strong className="current-temp">{Math.round(temp)}°C</strong>
             </div>
             <Row>
               <Col className="more-temps">
@@ -45,10 +63,10 @@ export default function Weather() {
             <div className="weather-attributes">
               <ul>
                 <li>
-                  Wind:<strong> 2 m/s</strong>
+                  Wind:<strong> {Math.round(wind)} km/s</strong>
                 </li>
                 <li>
-                  Humidity:<strong> 15%</strong>
+                  Humidity:<strong> {humidity}%</strong>
                 </li>
                 <li>
                   Pressure:<strong> 1061 hPa</strong>
