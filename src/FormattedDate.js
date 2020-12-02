06fc225b264697d32ed3dcf527df0766166668ae
LunaKawa <69447518+LunaKawa@users.noreply.github.com>
2020-12-01 21:32:04 -0800

    created date component

---
+++ src/FormattedDate.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+export default function FormattedDate(props) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[props.date.getDay()];
+  let months = [
+    "Jan",
+    "Feb",
+    "March",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sept",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = months[props.date.getMonth()];
+  let number = props.date.getDay();
+  return `${day} ${month} ${number}`;
+}
